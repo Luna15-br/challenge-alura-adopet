472e522beddb0b342d582370f3c0f7bdf63f01ca
Matheus Luna <matheusluna151634@gmail.com>
2025-03-21 10:48:23 -0300

    Configuração das dependências.

---
+++ api/src/test/java/lunab/adopet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lunab.adopet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
