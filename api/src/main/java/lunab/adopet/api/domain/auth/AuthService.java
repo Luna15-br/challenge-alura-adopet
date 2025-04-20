79d0638ade59e592fad7114c1a08452759d3c9b4
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:13:52 -0300

    Alteração no serviço de autenticação (AuthService)

--- api/src/main/java/lunab/adopet/api/domain/auth/AuthService.java
+++ api/src/main/java/lunab/adopet/api/domain/auth/AuthService.java
@@ -1,4 +1,20 @@
 package lunab.adopet.api.domain.auth;
 
-public class AuthService {
+import lunab.adopet.api.domain.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login);
+    }
 }
