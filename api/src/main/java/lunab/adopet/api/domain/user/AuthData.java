b9bc4324ed8439de5d40c7ce4330727303435b5e
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:15 -0300

    Modificação na classe AuthData (classe de domínio de dados de autenticação)

--- api/src/main/java/lunab/adopet/api/domain/user/AuthData.java
+++ api/src/main/java/lunab/adopet/api/domain/user/AuthData.java
@@ -2,14 +2,11 @@ package lunab.adopet.api.domain.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record AuthData(
-
         @NotBlank
         @Email
         String login,
         @NotBlank
         String password) {
-
 }
