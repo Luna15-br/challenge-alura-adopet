ed1b171977cf8763b7490b8e02f3353be2c58836
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:13:21 -0300

    Alteração no controlador de autenticação (AuthController)

--- api/src/main/java/lunab/adopet/api/controller/AuthController.java
+++ api/src/main/java/lunab/adopet/api/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/login")
-public class AuthManager {
+public class AuthController {
 
     @Autowired
     private AuthenticationManager manager;
