9ad9ba2eab512ff6f34470d86e9ecbbcb5760bca
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:23 -0300

    Modificação no repositório de usuários (UserRepository)

--- api/src/main/java/lunab/adopet/api/domain/user/UserRepository.java
+++ api/src/main/java/lunab/adopet/api/domain/user/UserRepository.java
@@ -1,6 +1,9 @@
 package lunab.adopet.api.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserData, Long> {
+
+    UserDetails findByLogin(String login);
 }
