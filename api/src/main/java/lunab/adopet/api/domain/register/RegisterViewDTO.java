321a92342a261b3a391bcecbe2bc3787a62e8ab1
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:09 -0300

    Atualização na classe RegisterViewDTO (data transfer object para registro)

--- api/src/main/java/lunab/adopet/api/domain/register/RegisterViewDTO.java
+++ api/src/main/java/lunab/adopet/api/domain/register/RegisterViewDTO.java
@@ -1,4 +1,18 @@
 package lunab.adopet.api.domain.register;
 
-public record RegisterViewDTO() {
+import lunab.adopet.api.domain.address.Address;
+import lunab.adopet.api.domain.user.UserData;
+
+import java.util.Optional;
+
+public record RegisterViewDTO(
+        Long id,
+        String nome,
+        String telefone,
+        String login,
+        Address address
+) {
+    public RegisterViewDTO (RegisterAdo registerAdo) {
+        this(registerAdo.getId(), registerAdo.getCpf(), registerAdo.getTel(), registerAdo.getUser().getUsername(), registerAdo.getAddress());
+    }
 }
