4856f9cfc9312083b48ed84e9e995b5558ec2116
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:13:47 -0300

    Atualização na classe Address (classe de domínio de endereço)

--- api/src/main/java/lunab/adopet/api/domain/address/Address.java
+++ api/src/main/java/lunab/adopet/api/domain/address/Address.java
@@ -4,21 +4,17 @@ package lunab.adopet.api.domain.address;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Embeddable
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Address {
 
     private String uf;
     private String city;
 
+    public Address () {
+
+    }
 
     public Address(@NotNull @Valid DataAddress address) {
         this.uf = address.uf();
