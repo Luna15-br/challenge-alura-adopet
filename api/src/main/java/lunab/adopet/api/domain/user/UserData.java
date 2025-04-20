2f9cb3cfdcf8783e2dfd5ce12279506c47273748
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:20 -0300

    Alteração na classe UserData (classe de domínio de dados de usuário)

--- api/src/main/java/lunab/adopet/api/domain/user/UserData.java
+++ api/src/main/java/lunab/adopet/api/domain/user/UserData.java
@@ -2,23 +2,73 @@ package lunab.adopet.api.domain.user;
 
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "user")
 @Entity
-@Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
-public class UserData {
+public class UserData implements UserDetails {
 
     @Id
     private String login;
     private String password;
 
 
+    public UserData () {
+
+    }
     public UserData(AuthData authData) {
         this.login = authData.login();
         this.password = authData.password();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
