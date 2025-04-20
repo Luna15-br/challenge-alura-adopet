2289e1143116d54329c4c0be7db04411e795ca8a
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:27 -0300

    Alteração no tratamento de erros (ErrorHandling) da aplicação

--- api/src/main/java/lunab/adopet/api/infra/exception/ErrorHandling.java
+++ api/src/main/java/lunab/adopet/api/infra/exception/ErrorHandling.java
@@ -10,16 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandling {
+
     @ExceptionHandler (EntityNotFoundException.class)
     public ResponseEntity error404() {
         return ResponseEntity.notFound().build();
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity error400(MethodArgumentNotValidException exception) {
         var error = exception.getFieldErrors();
-
         return ResponseEntity.badRequest().body(error.stream().map(DataErrorValidation::new).toList());
     }
+
     private record DataErrorValidation(String entry, String message) {
         public DataErrorValidation (FieldError error) {
             this(error.getField(), error.getDefaultMessage());
