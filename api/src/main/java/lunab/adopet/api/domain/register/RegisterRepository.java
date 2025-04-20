a58b1eb6618b1e337aaf85a1611e04764a8792a5
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:02 -0300

    Alteração no repositório de registro (RegisterRepository)

--- api/src/main/java/lunab/adopet/api/domain/register/RegisterRepository.java
+++ api/src/main/java/lunab/adopet/api/domain/register/RegisterRepository.java
@@ -1,6 +1,10 @@
 package lunab.adopet.api.domain.register;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface RegisterRepository extends JpaRepository<RegisterAdo, Long> {
+  RegisterAdo findByUser_Login(String login);
 }
