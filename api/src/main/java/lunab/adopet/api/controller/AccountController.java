92c5456c40b3370a500df392015b6d1413845ae5
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:13:15 -0300

    Alteração no controlador de contas (AccountController)

--- api/src/main/java/lunab/adopet/api/controller/AccountController.java
+++ api/src/main/java/lunab/adopet/api/controller/AccountController.java
@@ -1,4 +1,33 @@
 package lunab.adopet.api.controller;
 
+import lunab.adopet.api.domain.register.RegisterRepository;
+import lunab.adopet.api.domain.register.RegisterViewDTO;
+import lunab.adopet.api.domain.user.UserData;
+import lunab.adopet.api.domain.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/account")
 public class AccountController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RegisterRepository registerRepository;
+
+
+    @GetMapping("/me")
+    public ResponseEntity<RegisterViewDTO> getProfile(@AuthenticationPrincipal UserData user) {
+        var register = registerRepository.findByUser_Login(user.getUsername());
+        var dto = new RegisterViewDTO(register);
+        return ResponseEntity.ok(dto);
+    }
+
+
+
 }
