9c883fe57fd389abf5fe683f07be8eaf451b6cdd
Matheus Luna <matheusluna151634@gmail.com>
2025-04-20 16:14:35 -0300

    Alteração no serviço de token JWT (TokenService)

--- api/src/main/java/lunab/adopet/api/infra/security/TokenService.java
+++ api/src/main/java/lunab/adopet/api/infra/security/TokenService.java
@@ -1,4 +1,47 @@
 package lunab.adopet.api.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import lunab.adopet.api.domain.user.UserData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String gerarToken(UserData user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Rest AdoPet")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar o token", e);
+        }
+    }
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Rest AdoPet")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
 }
