621c3abf39da01b92b8f7bd84e243d6739d83489
Matheus Luna <matheusluna151634@gmail.com>
2025-04-21 15:16:15 -0300

    feat: adiciona endpoint PUT /account/me para edição de perfil

--- api/src/main/java/lunab/adopet/api/controller/AccountController.java
+++ api/src/main/java/lunab/adopet/api/controller/AccountController.java
@@ -1,5 +1,8 @@
 package lunab.adopet.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import lunab.adopet.api.domain.register.RegisterPut;
 import lunab.adopet.api.domain.register.RegisterRepository;
 import lunab.adopet.api.domain.register.RegisterViewDTO;
 import lunab.adopet.api.domain.user.UserData;
@@ -7,12 +10,11 @@ import lunab.adopet.api.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping ("/account")
+@RequestMapping ("/account/me")
 public class AccountController {
 
     @Autowired
@@ -21,12 +23,20 @@ public class AccountController {
     private RegisterRepository registerRepository;
 
 
-    @GetMapping("/me")
+    @GetMapping
     public ResponseEntity<RegisterViewDTO> getProfile(@AuthenticationPrincipal UserData user) {
         System.out.println("Usuário autenticado: " + user.getUsername());
         var register = registerRepository.findByUser_Login(user.getUsername());
         var dto = new RegisterViewDTO(register);
         return ResponseEntity.ok(dto);
+    }
+    @PutMapping
+    @Transactional
+    public ResponseEntity<?> putProfile(@RequestBody @Valid RegisterPut put) {
+        var authenticatedUser = (UserData) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var register = registerRepository.findByUser_Login(authenticatedUser.getUsername());
+        register.atualizarInfo(put);
+        return ResponseEntity.ok(new RegisterViewDTO(register));
     }
 
 
