c7c376e766fe7d58218755adc782458c655c551c
Matheus Luna <matheusluna151634@gmail.com>
2025-04-21 14:06:33 -0300

    feat(register): adicionar DTO de visualização para retorno seguro de dados do registro

--- api/src/main/java/lunab/adopet/api/domain/register/RegisterViewDTO.java
+++ api/src/main/java/lunab/adopet/api/domain/register/RegisterViewDTO.java
@@ -1,18 +1,17 @@
 package lunab.adopet.api.domain.register;
 
 import lunab.adopet.api.domain.address.Address;
-import lunab.adopet.api.domain.user.UserData;
-
-import java.util.Optional;
 
 public record RegisterViewDTO(
         Long id,
-        String nome,
-        String telefone,
+        String name,
+        String tel,
         String login,
-        Address address
+        String username,
+        String city,
+        String uf
 ) {
     public RegisterViewDTO (RegisterAdo registerAdo) {
-        this(registerAdo.getId(), registerAdo.getCpf(), registerAdo.getTel(), registerAdo.getUser().getUsername(), registerAdo.getAddress());
+        this(registerAdo.getId(), registerAdo.getName(), registerAdo.getCpf(), registerAdo.getTel(), registerAdo.getUser().getUsername(), registerAdo.getAddress().getCity(), registerAdo.getAddress().getUf());
     }
 }
